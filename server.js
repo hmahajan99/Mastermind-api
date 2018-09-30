bf759301610bba06381d5a6379643221e6b7d57d
Harshit Mahajan <hmahajan1999@gmail.com>
2018-09-30 16:09:11 +0530

    updated connection

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const leaderboard = require('./controllers/leaderboard');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl: true
   }
 });
